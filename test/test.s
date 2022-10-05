bbf475fe28d186ceeaeb419f9d3c82c09bd79384
Emmett Collings <emmett.collings@gmail.com>
2022-10-05 11:06:24 -0600

    update some messing around

--- test/test.s
+++ test/test.s
@@ -4,15 +4,14 @@
  */
     processor 6502          ; tell dasm we are writing 6502 asm
 
-
 /* 
  * Write some BASIC code into memory that will jump to our assembly. User
  * written BASIC gets stored at $1001 so that's where we begin
  */
     org     $1001           ; mem location assembler assembles to
     dc.w    stubend
-    dc.w    1
-    dc.b    $9e, "4109", 0  ; allocate bytes
+    dc.w    1               ; arbitrary line number for BASIC syntax
+    dc.b    $9e, "4109", 0  ; allocate bytes. 4109 = $100d
 
 stubend:    
     dc.w    0               ; insert null byte
@@ -23,3 +22,4 @@ start:
     lda     #$30            ; this is the VIC-20 symbol for '0'
     jsr     CHROUT          ; call CHROUT 
     rts                     ; return to caller
+
