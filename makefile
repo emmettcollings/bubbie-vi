39637733e60634e7ac598366fcf2c1e294a8bf05
Ricky <rickyb5@live.ca>
2022-10-07 21:11:31 -0600

    Borrow Emmetts code to ensure my compiler is actually working. Make a testing makefile. Make a .gitingore

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 DASM = dasm/dasm.exe
 
 test1: test1.s
-	$(DASM) test1.s -f3 -l test1.lst -o test1.bin
+	$(DASM) test1.s -obin/test1.prg -lbin/test1.lst
--- test1.s
+++ test1.s
@@ -3,3 +3,21 @@
 */
     processor 6502 ; This informs DASM we are assembling for the 6502 processor.
 
+/* 
+ * Write some BASIC code into memory that will jump to our assembly. User
+ * written BASIC gets stored at $1001 so that's where we begin
+ */
+    org     $1001           ; mem location assembler assembles to
+    dc.w    stubend
+    dc.w    1               ; arbitrary line number for BASIC syntax
+    dc.b    $9e, "4109", 0  ; allocate bytes. 4109 = $100d
+
+stubend:    
+    dc.w    0               ; insert null byte
+
+CHROUT = $ffd2              ; KERNAL routine we use for testing
+
+start: 
+    lda     #$30            ; this is the VIC-20 symbol for '0'
+    jsr     CHROUT          ; call CHROUT 
+    rts                     ; return to caller
