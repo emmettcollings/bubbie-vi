23f827f3e4440a2354ca66d297bdca814e3eab42
Ricky <rickyb5@live.ca>
2022-10-09 15:28:38 -0600

    Create Title Screen test.

---
+++ testTitleScreen.s
@@ -0,0 +1,33 @@
+/*
+    Processor Information
+*/
+    processor   6502        ; This informs the assembler that we are using a 6502 processor.
+
+/*
+    Memory Map
+*/
+    org     $1001           ; mem location of user region
+    dc.w    stubend
+    dc.w    1               ; arbitrary line number for BASIC syntax
+    dc.b    $9e, "4353", 0  ; allocate bytes. 4353 = 1101
+
+/* 
+    Global Definitions
+*/
+CHROUT = $ffd2          ; kernal character output routine
+CLS = $e55f             ; kernal clear screen routine
+
+/*
+    Utility Routines
+*/
+stubend:
+    dc.w    0               ; insert null byte
+
+/*
+    Main Routine
+*/
+    org     $1101           ; mem location of code region
+start: 
+    lda     #$30            ; this is the VIC-20 symbol for '0'
+    jsr     CHROUT          ; call CHROUT
+    rts                     ; return to caller
