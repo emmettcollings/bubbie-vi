a7b75c73b2387a57b3c79ff975aa20c3ca5f4d3a
Ricky <rickyb5@live.ca>
2022-10-09 15:54:48 -0600

    Get majority of the titleScreen test done

--- testTitleScreen.s
+++ testTitleScreen.s
@@ -1,4 +1,14 @@
 /*
+    Title Screen Test
+
+    This test will display the title screen to the user.
+    Current TODO List:
+        - Check the output, seems as a few characters are wrong.
+        - Figure out a good way to horizontally center the text. (We know how to shift left/right, just want to "automate" it if possible, otherwise we can hardcode it easily.)
+        - Figure out how to center it vertically. (No clue how to shift up/down yet...)
+*/
+
+/*
     Processor Information
 */
     processor   6502        ; This informs the assembler that we are using a 6502 processor.
@@ -24,10 +34,61 @@ stubend:
     dc.w    0               ; insert null byte
 
 /*
+    Data
+*/
+l100d   .byte   $c2, $55, $42, $42, $49, $45, $20, $54, $48, $45, $20, $d6, $69 ; Bubbie the VI (Length: 13)
+l100e   .byte   $CE, $4f, $54, $20, $c5, $4e, $4f, $55, $47, $48, $20, $cd, $45, $4d, $4f, $52, $59 ; Not Enough Memory (Length: 17)
+l100f   .byte   $32, $30, $32, $32 ; 2022 (Length: 4)
+
+/*
     Main Routine
 */
     org     $1101           ; mem location of code region
 start: 
-    lda     #$30            ; this is the VIC-20 symbol for '0'
-    jsr     CHROUT          ; call CHROUT
+    jsr     CLS             ; clear screen
+
+    ldx     #$00            ; set x register to 0
+    jsr     printGameName   ; print game name
+
+    jsr     resetOutput     ; reset output before next line
+
+    ldx     #$00            ; set x register to 0
+    jsr     printTeamName   ; print team name
+
+    jsr     resetOutput     ; reset output before next line
+
+    ldx     #$00            ; set x register to 0
+    jsr     printYear       ; print year
+
+    rts                     ; return to caller
+
+resetOutput:
+    lda     #$11            ; load new line character
+    jsr     CHROUT          ; print a newline
+    lda     #$00            ; load 0 to reset column shift
+    sta     $00d3           ; set column shift to 0
+    rts
+
+printGameName:
+    lda     l100d,x         ; load byte from game name string
+    jsr     CHROUT          ; output the byte
+    inx                     ; increment x register
+    cpx     #$0d            ; compare x register to the length of the game name string (find a way to automate this if possible?)
+    bne     printGameName   ; if not equal, jump back to the top of the routine
+    rts                     ; return to caller
+
+printTeamName:
+    lda     l100e,x         ; load a byte from the team name string
+    jsr     CHROUT          ; output the byte
+    inx                     ; increment x register
+    cpx     #$11            ; compare x register to the length of the team name string (find a way to automate this if possible?)
+    bne     printTeamName   ; if not equal, jump back to the top of the routine
+    rts                     ; return to caller
+
+printYear:
+    lda     l100f,x         ; load a byte from the year string
+    jsr     CHROUT          ; output the byte
+    inx                     ; increment x register
+    cpx     #$04            ; compare x register to the length of the year string (find a way to automate this if possible?)
+    bne     printYear       ; if not equal, jump back to the top of the routine
     rts                     ; return to caller
