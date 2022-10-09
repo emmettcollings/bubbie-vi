70cca59f6f571f3c25b320b69b77a062ca7f43d5
Ricky <rickyb5@live.ca>
2022-10-09 13:40:03 -0600

    Update template.

--- template_test.s
+++ template_test.s
@@ -11,6 +11,11 @@
     dc.w    1               ; arbitrary line number for BASIC syntax
     dc.b    $9e, "4353", 0  ; allocate bytes. 4353 = 1101
 
+/* 
+    Global Definitions
+*/
+    CHROUT = $ffd2          ; kernal character output routine 
+
 /*
     Utility Routines
 */
@@ -22,5 +27,5 @@ stubend:
 */
 start: 
     lda     #$30            ; this is the VIC-20 symbol for '0'
-    jsr     $ffd2           ; call CHROUT (The KERNAL routine for printing a character)
+    jsr     CHROUT          ; call CHROUT
     rts                     ; return to caller
