80e80c0ba1093fa16683dd931b5fc8230798c5b5
Emmett Collings <emmett.collings@gmail.com>
2022-10-10 07:21:24 -0600

    Add busyLoop subroutine to lib.
    See the readkeys test for an example usage

---
+++ lib/busyLoop.s
@@ -0,0 +1,19 @@
+/*
+ * A simple busyloop that might be useful in a program's main loop
+ *
+ * Input: # of iterations to cycle in a
+ */
+busyLoop:
+    tax     ; transfer input to x
+
+body
+    SUBROUTINE
+
+loop:
+    nop             ; waste time
+    dex             ; decrement counter
+    cpx     #0
+    bne     loop    ; keep looping if we haven't reached the end
+    rts
+
+
--- lib/lib.s
+++ lib/lib.s
@@ -1,8 +1,13 @@
 /* lib.s
  * A big thing that includes all of the subroutines we make so that we only 
  * need one include in tests for now. May have some weird multiple includes 
- * thing, not sure if dasm handles this nicely
+ * thing, not sure if dasm handles this nicely.
+ *
+ * Considerations to think about:
+ * - need to make better use of SUBROUTINE dasm directive so we don't have to 
+ *   be super anal about unique labels
  */
 
 include "globals.s"
 include "printString.s"
+include "busyLoop.s"
