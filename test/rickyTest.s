56eaf246ed40b3471ae0a97cae6f076b69236839
Ricky <rickyb5@live.ca>
2022-10-21 15:27:27 -0600

    Some notes and testing.

--- test/rickyTest.s
+++ test/rickyTest.s
@@ -18,13 +18,20 @@
 stubend:    
     dc.w    0               ; insert null byte
 
+/* 
+    Global Definitions
+*/
+CLS = $e55f                 ; kernal clear screen routine
+COL_MEM = $9400             ; Color memory location
+
 /*
     Main Program
 */
     org     $1101           ; mem location of code region
 start: 
+    jsr     CLS             ; clear screen
     lda     #$ac            ; funny colors 
-    jsr     colorShift
+    jmp     colorShift
     rts                     ; return to caller
 
 /*
@@ -33,4 +40,15 @@ start:
  */
 colorShift:
     sta     $900f           ; location of screen and border color stuff
-    rts
+    ; store 191 as hex in accumulator
+    lda     #$bf
+    sta     $9600
+    jmp     colorShift
+
+/* 
+    Note Section
+    -> Apparently the formula to find the current location of color memory is:
+        C = 37888 + 4 * (PEAK (36866) AND 128) [This formula is obviously in BASIC]
+        So, the formula in 6502 ASM is:
+        C = $9400 + 4 * ($9000 AND $80) [Obviously, this isn't right, just noting it down in a basic sense]
+    
