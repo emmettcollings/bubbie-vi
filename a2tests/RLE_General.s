22d493215498850d061d76b9c233a8dc469f4487
bryce219 <41980089+bryce219@users.noreply.github.com>
2022-11-02 19:54:52 -0600

    uh i think i found one of the bugs aycock was talking about

--- a2tests/RLE_General.s
+++ a2tests/RLE_General.s
@@ -17,7 +17,7 @@
     org     $1001           ; mem location of user region
     dc.w    stubend
     dc.w    1               ; arbitrary line number for BASIC syntax
-    dc.b    $9e, "4142", 0  ; allocate bytes. 4125 = 101d
+    dc.b    $9e, "4109", 0  ; allocate bytes.
 
 /* 
     Global Definitions
@@ -34,29 +34,6 @@ stubend:
     dc.w    0               ; insert null byte
 
 /*
-    Data
-    _ - 0
-    0 - 1
-    2 - 2
-    b - 3
-    e - 4
-    g - 5
-    h - 6
-    i - 7
-    m - 8
-    n - 9
-    o - a
-    r - b
-    t - c
-    u - d
-    v - e
-    y - f
-*/
-lC100d   .byte   $20, $30, $32, $02, $05, $07, $08, $09, $0d, $0e, $0f, $12, $14, $15, $16, $19
-
-lC101d   .byte   $3d, $33, $74, $0c, $64, $0e, $79, $ac, $04, $9a, $d5, $60, $84, $8a, $bf, $21, $22
-
-/*
     Main Routine
 */
 start: 
@@ -65,57 +42,29 @@ start:
     lda     #$06        ; blue
     jsr     colorScreen
 
-    lda     #$0d
-    sta     $fb
-
-    lda     #$10
-    sta     $fc
-
-    ldx     #$00
-decompress:
-    lda     $101d,x
-    lsr
-    lsr
-    lsr
-    lsr
-    pha
-    lda     $101d,x
-    and     #%00001111
-    pha
-
-    inx
-    cpx     #$11
-    bne     decompress
-
-    ldx     #$3
-writeYear:
-    pla
-    tay
-    lda     ($fb),y
-    sta     SCRMEM+$76,x
+    ldy     #$00
+    sty     $fc
+L2:
+    lda     $1102,y
+    cmp     #$00
+    beq     justinWantsInf
+    tax
+    lda     $1101,y
+    iny
+    iny
+    sty     $fb
+    ldy     $fc
+L1:
+    sta     $1e00,y
+    iny
     dex
-    bpl     writeYear
-
-    ldx     #$10
-writeName:
-    pla
-    tay
-    lda     ($fb),y
-    sta     SCRMEM+$5b,x
-    dex
-    bpl     writeName
-
-    ldx     #$0c
-writeTitle:
-    pla
-    tay
-    lda     ($fb),y
-    sta     SCRMEM+$31,x
-    dex
-    bpl     writeTitle
+    bne     L1
+    sty     $fc
+    ldy     $fb
+    bne     L2
 
 justinWantsInf:
-    jmp    justinWantsInf   ; yeeehhhhawwwww!
+    beq    justinWantsInf   ; yeeehhhhawwwww!
 
 /*
  * Writes whatever is in a to 512 bytes of color mem
@@ -130,6 +79,7 @@ colorLoop:
     bne     colorLoop
     rts
 
+
 clearScreen:
     ldx     #$00    ; only have 1 byte that we can loop on
     lda     #$20    ; clear the screen
@@ -139,3 +89,34 @@ clearScreen:
     inx
     bne     .loop
     rts
+
+/*
+    Data
+    _ - 20
+    0 - 30
+    2 - 32
+    b - 02
+    e - 05
+    g - 07
+    h - 08
+    i - 09
+    m - 0d
+    n - 0e
+    o - 0f
+    r - 12
+    t - 14
+    u - 15
+    v - 16
+    y - 19
+*/
+
+    org     $1101
+
+    dc.b    $20, $31, $02, $01, $15, $01, $02, $02, $09, $01, $05, $01, $20, $01    ; BUBBIE
+    dc.b    $14, $01, $08, $01, $05, $01, $20, $01                                  ; THE
+    dc.b    $16, $01, $09, $01, $20, $1d                                            ; VI
+
+    dc.b    $0e, $01, $0f, $01, $14, $01, $20, $01                                  ; NOT
+    dc.b    $05, $01, $0e, $01, $0f, $01, $15, $01, $07, $01, $08, $01              ; ENOUGH
+    ; ;dc.b    "MEMORY"                                                                ; MEMORY
+
