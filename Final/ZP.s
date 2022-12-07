47ff9cf73b2309b9ae48ca0bdcbbd86707f8988b
Ricky <rickyb5@live.ca>
2022-12-07 11:06:38 -0700

    comment update

--- Final/ZP.s
+++ Final/ZP.s
@@ -1,6 +1,5 @@
-; $fb-fe are to always be considered free temporary space for whatever variable is needed
-; at the time, so there is no real point in giving them distinct names
-
+; Temporary Storage
+;   - $fb-fe are to always be considered free temporary space for whatever variable is needed
 TEMP1 = $fb
 TEMP2 = $fc
 TEMP3 = $fd
