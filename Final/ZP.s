0eeddf1da464644ce02eb1d6e9995a2cb9a3e633
Ricky <rickyb5@live.ca>
2022-12-07 11:24:14 -0700

    add more temp storage vars

--- Final/ZP.s
+++ Final/ZP.s
@@ -4,3 +4,10 @@ TEMP1 = $fb
 TEMP2 = $fc
 TEMP3 = $fd
 TEMP4 = $fe
+
+; Temporary Storage
+;   - $8d-8f are to always be considered free temporary space for whatever variable is needed
+;   - These ZP locations are used by BASIC's RND function, which we do not use.
+TEMP5 = $8d
+TEMP6 = $8e
+TEMP7 = $8f
